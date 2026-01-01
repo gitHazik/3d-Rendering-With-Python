1bb186a3d253d1ac65abc11a687c5289795c2ab2
gitHazik <hazikkhalid700@gmail.com>
2026-01-01 22:35:59 +0530

    render cube with rotation

--- main.py
+++ main.py
@@ -2,5 +2,42 @@ from ursina import *
 
 
 app = Ursina()
-app.run()
 
+
+window.title = "Basic 3D rendering"
+window.borderless= False
+window.fullscreen= False
+window.exit_button.visible= False
+window.fps_counter.enable= False
+
+
+cube = Entity(
+            model= 'cube',
+            color=color.orange,
+            scale= (2,2,2),
+            position = (0,0,0)
+        )
+
+
+
+
+
+
+
+
+def update():
+    cube.rotation_z += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
