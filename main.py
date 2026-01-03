27b479a342e3b866916c6d81c6dbb1a8793e0909
gitHazik <hazikkhalid700@gmail.com>
2026-01-03 14:50:52 +0530

    port from Panda3D to Ursina

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import *
-from panda3d.core import LColor, CardMaker
-from direct.task import Task
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-class App(ShowBase):
-    def __init__(self):
-        super().__init__()
-        
-        self.disable_mouse()
-        
-        # Setup camera
-        self.camera.set_pos(0, -20, 5)
-        
-        # Mouse control
-        self.mouse_sensitivity = 0.2
-        self.camera_heading = 0
-        self.camera_pitch = 0
-        
-        # Create simple scene
-        self.create_scene()
-        
-    
-    def create_scene(self):
-        # Ground
-        cm = CardMaker("ground")
-        cm.set_frame(-30, 30, -30, 30)
-        ground = self.render.attach_new_node(cm.generate())
-        ground.set_p(-90)
-        ground.set_z(-2)
-        ground.set_color(LColor(1, 1, 1, 1))
-        
+app = Ursina()
 
+# Ground / Platform
+ground = Entity(
+    model='plane',
+    scale=(50, 1, 50),
+    texture='white_cube',
+    texture_scale=(50, 50),
+    collider='box'
+)
 
-app = App()
+# FPS Player (Camera + Controller)
+player = FirstPersonController(
+    y=2,          # player height above ground
+    speed=5,      # movement speed
+    jump_height=2,
+    gravity=1
+)
+
+# Optional sky
+Sky()
+
 app.run()
