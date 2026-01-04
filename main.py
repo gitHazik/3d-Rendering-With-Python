98a4af77fa7e06e1f46e712d60d1e70f67737257
gitHazik <hazikkhalid700@gmail.com>
2026-01-04 11:56:01 +0530

    init

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from direct.showbase.ShowBase import ShowBase
 from Scripts.fps_controller import FPSController
-
+from Scripts.sky import Sky
 
 class Game(ShowBase):
     def __init__(self):
         super().__init__()
         self.disableMouse()
-
+        tp= 'assets/Sky/sky.png'
+        Sky(self,tp)
         self.player = FPSController(self)
 
 
